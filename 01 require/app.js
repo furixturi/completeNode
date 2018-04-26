05c1db5ebb7ace5457fcb15310ca55b043771536
furixturi <alabebop@gmail.com>
2018-04-26 21:43:56 +0900

    Use required local module

--- 01 require/app.js
+++ 01 require/app.js
@@ -3,3 +3,7 @@ console.log('Starting app.');
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js');
+
+var user = os.userInfo();
+console.log(user);
+fs.appendFile('greetings.txt', `Hello ${user.username}, you are ${notes.age} years old!\n`);
