484a6e508e4165d2dd52da2699b1d655144a5f1a
furixturi <alabebop@gmail.com>
2018-05-05 09:50:28 +0900

    Add playground for json

---
+++ 03 fundamentals/playground/json.js
@@ -0,0 +1,7 @@
+var obj = {
+    name: 'Xiaoli'
+};
+
+var stringObj = JSON.stringify(obj);
+console.log('=> typeof stringObj:\n', typeof stringObj);
+console.log('=> content of stringObj:\n', stringObj);
