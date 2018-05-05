71bbfc76768533828ccd09455b1f03ed400214a0
furixturi <alabebop@gmail.com>
2018-05-05 09:46:18 +0900

    parse yargs.argv

--- 03 fundamentals/notes-app/app.js
+++ 03 fundamentals/notes-app/app.js
@@ -7,22 +7,20 @@ const yargs = require('yargs');
 const notes = require('./notes.js');
 
 const argv = yargs.argv;
+const command = argv._[0];
 
-console.log("yargs.argv: ", yargs.argv);
-
-const command = yargs.argv['_'][0];
 switch(command){
     case 'add':
-        notes.addNote(yargs.argv.title, yargs.argv.body);
+        notes.addNote(argv.title, argv.body);
         return;
     case 'list':
         notes.getAll();
         return;
     case 'read':
-        notes.getNote(yargs.argv.title);
+        notes.getNote(argv.title);
         return;
     case 'remove':
-        notes.removeNote(yargs.argv.title);
+        notes.removeNote(argv.title);
         return;
     default:
         console.log("Command not recognized");
