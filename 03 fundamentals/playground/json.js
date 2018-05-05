31b4f413bfbe1c7387af2061e4f73d2d1a8c4b91
furixturi <alabebop@gmail.com>
2018-05-05 10:01:03 +0900

    write json to a file and read it back in the program

--- 03 fundamentals/playground/json.js
+++ 03 fundamentals/playground/json.js
@@ -1,7 +1,27 @@
-var obj = {
-    name: 'Xiaoli'
-};
+// var obj = {
+//     name: 'Xiaoli'
+// };
 
-var stringObj = JSON.stringify(obj);
-console.log('=> typeof stringObj:\n', typeof stringObj);
-console.log('=> content of stringObj:\n', stringObj);
+// var stringObj = JSON.stringify(obj);
+// console.log('=> typeof stringObj:\n', typeof stringObj);
+// console.log('=> content of stringObj:\n', stringObj);
+
+// var personString = '{"name": "Xiaoli", "age": 25}';
+// var person = JSON.parse(personString);
+// console.log('=> typeof person:\n', typeof person);
+// console.log('=> content of person:\n', person);
+
+
+const fs = require('fs');
+
+var originalNote = {
+    title: 'Some title',
+    body: 'Some body texts'
+};
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.js', originalNoteString);
+
+var noteString = fs.readFileSync('notes.js');
+var note = JSON.parse(noteString);
+console.log('=> typeof note:\n', typeof note)
+console.log('=> content of note:\n', note)
