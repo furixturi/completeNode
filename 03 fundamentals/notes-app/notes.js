2840887f7d80ad35f172efe5e7d7c776e95aeca7
furixturi <alabebop@gmail.com>
2018-05-05 10:24:18 +0900

    write new note to notes.json if there isn't one with the same title yet

--- 03 fundamentals/notes-app/notes.js
+++ 03 fundamentals/notes-app/notes.js
@@ -1,9 +1,27 @@
 console.log('Starting notes.js');
+const fs = require('fs');
 
 const addNote = (title, body) => {
     console.log('========== Adding note ========\n', 
         'title:', title, 
         'body:', body);
+    let notes = [];
+    const note = {title, body};
+
+    try {
+        const notesString = fs.readFileSync('./notes.json');
+        notes = JSON.parse(notesString);
+    } catch (e) {
+
+    }
+
+    if(notes.filter(note => note.title === title).length === 0) {
+        notes.push(note);
+        fs.writeFileSync("./notes.json", JSON.stringify(notes));
+    } else {
+        console.log('note with title', title, 'already exists.')
+    }
+   
 }
 
 const getAll = () => {
