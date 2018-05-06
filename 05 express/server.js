77e6ee6a9bedd74f9886f056d64d0f0ada681acf
furixturi <alabebop@gmail.com>
2018-05-06 21:29:32 +0900

    Add a log middleware

--- 05 express/server.js
+++ 05 express/server.js
@@ -2,6 +2,22 @@ const express = require("express");
 
 const app = express();
 
+
+
+// app.use let us use middleware
+// The built-in express.static middleware takes the absolute path to the folder
+app.use(express.static(__dirname + '/public'))
+
+// Custom middleware
+app.use((req, res, next) => {
+  const now = new Date().toString();
+
+  console.log(`========= New request ${now} ==========`)
+  console.log(`method: ${req.method}, url: ${req.url}`)
+  next();
+});
+
+
 app.get("/", (req, res) => {
   // res.send("<h1>Hello express!</h1>");
   res.send({
@@ -10,13 +26,9 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get('/about', (req, res) => {
-  res.send('About page')
+app.get("/about", (req, res) => {
+  res.send("About page");
 });
-
-// app.use let us use middleware
-// The built-in express.static middleware takes the absolute path to the folder
-app.use(express.static(__dirname + '/public'))
 
 app.listen(3000, () => {
   console.log('Server is up on port 3000');
