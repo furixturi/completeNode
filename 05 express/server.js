29cec8277d841f5fef401d750cbf5f9ccdb57de9
furixturi <alabebop@gmail.com>
2018-05-06 16:01:49 +0900

    send http and json to different routes

--- 05 express/server.js
+++ 05 express/server.js
@@ -1,9 +1,17 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send("<h1>Hello express!</h1>");
+app.get("/", (req, res) => {
+  // res.send("<h1>Hello express!</h1>");
+  res.send({
+    name: "Xiaoli",
+    likes: ["eating", "travel"]
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.send('About page')
 });
 
 app.listen(3000);
