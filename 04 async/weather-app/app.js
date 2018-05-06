e9ffb8a5f9d5e3c261ae2f4a054b441e90ffdadb
furixturi <alabebop@gmail.com>
2018-05-06 12:09:33 +0900

    take user input of address in console

--- 04 async/weather-app/app.js
+++ 04 async/weather-app/app.js
@@ -13,36 +13,38 @@ const argv = yargs
   .help()
   .alias("help", "h").argv;
 
-  console.log(argv);
+const addressInput = argv.address;
+const addressURIEncoded = encodeURIComponent(addressInput);
+const apiKey = "AIzaSyCyoH0zQlgSU6DjtQGtVvyWtiImXYYVlWE";
+const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${addressURIEncoded}&key=${apiKey}`;
+console.log(url);
 
-// const apiKey = "AIzaSyCyoH0zQlgSU6DjtQGtVvyWtiImXYYVlWE";
-// request(
-//   {
-//     url:
-//       "https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia",
-//     json: true
-//   },
-//   (err, res, body) => {
-//     console.log("error: ", err);
-//     console.log(JSON.stringify(res, undefined, 2));
+request(
+  {
+    url: url,
+    json: true
+  },
+  (err, res, body) => {
+    console.log("error: ", err);
+    // console.log(JSON.stringify(res, undefined, 2));
 
-//     if (body.status === "OK") {
-//       console.log("============= OjbK ============");
-//       const result = body.results[0];
-//       console.log("address:", result.formatted_address);
-//       console.log(
-//         "latitude:",
-//         result.geometry.location.lat,
-//         "longitude:",
-//         result.geometry.location.lng
-//       );
-//     } else {
-//       console.log("========= Panic!!! =========");
-//       console.log("Error status:", body.status);
-//       console.log(
-//         "Friendly yet confusing message from Google:",
-//         body.error_message
-//       );
-//     }
-//   }
-// );
+    if (body.status === "OK") {
+      console.log("============= OjbK ============");
+      const result = body.results[0];
+      console.log("address:", result.formatted_address);
+      console.log(
+        "latitude:",
+        result.geometry.location.lat,
+        "longitude:",
+        result.geometry.location.lng
+      );
+    } else {
+      console.log("========= Panic!!! =========");
+      console.log("Error status:", body.status);
+      console.log(
+        "Friendly yet confusing message from Google:",
+        body.error_message
+      );
+    }
+  }
+);
