ca39c478d9357e0290b6b8814367b9f8586667f3
furixturi <alabebop@gmail.com>
2018-05-07 09:55:38 +0900

    Test async function

--- 06-testing/utils/utils.test.js
+++ 06-testing/utils/utils.test.js
@@ -14,3 +14,13 @@ it("should expect some values", () => {
   expect({ a: 1, b: 2 }).toInclude({ a: 1 });
   expect([1, 2, 3]).toInclude(1);
 });
+
+// Mocha knows it is async as long as a done callback is passed in
+it("should add two numbers asynchronously", (done) => {
+  const res = utils.asyncAdd(1, 2, sum => {
+    expect(sum)
+      .toBeA("number")
+      .toBe(3);
+    done();
+  });
+});
