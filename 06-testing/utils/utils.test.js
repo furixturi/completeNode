47162d717c5f0c833a64369e34ef3aa84c0311d9
furixturi <alabebop@gmail.com>
2018-05-07 09:46:36 +0900

    commonly used assertion

--- 06-testing/utils/utils.test.js
+++ 06-testing/utils/utils.test.js
@@ -1,7 +1,16 @@
-const expect = require('expect');
+const expect = require("expect");
 const utils = require("./utils");
 
 it("should add two numbers", () => {
   const res = utils.add(33, 11);
-  expect(res).toBeA('number').toBe(44);
+  expect(res)
+    .toBeA("number")
+    .toBe(44);
+});
+
+it("should expect some values", () => {
+  // To compare objects, do not use toBe, but use toEqual
+  expect({ name: "Tuzi" }).toEqual({ name: "Tuzi" });
+  expect({ a: 1, b: 2 }).toInclude({ a: 1 });
+  expect([1, 2, 3]).toInclude(1);
 });
