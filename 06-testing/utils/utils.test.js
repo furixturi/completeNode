c3ace1044611832594817b6457b16bee9ddeaaf4
furixturi <alabebop@gmail.com>
2018-05-07 09:41:55 +0900

    Use expect as assertion library

--- 06-testing/utils/utils.test.js
+++ 06-testing/utils/utils.test.js
@@ -1,6 +1,7 @@
+const expect = require('expect');
 const utils = require("./utils");
 
 it("should add two numbers", () => {
   const res = utils.add(33, 11);
-  if(res !== 44) throw new Error(`Expect value to be 44 but got ${res}`);
+  expect(res).toBeA('number').toBe(44);
 });
