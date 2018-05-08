3c31c11a06d0f8fd00feb48bb4a6eadef177bfd9
Xiaoli Shen <xiaoli.shen@fastretailing.com>
2018-05-08 16:14:15 +0900

    install supertest and refactor to separate server and app logic for integration test

--- jest-test-express/src/index.js
+++ jest-test-express/src/index.js
@@ -1,10 +1,6 @@
 /* eslint-disable no-console */
-const express = require('express');
-
-const hello = require('./hello');
+const app = require('./app');
 const handleListen = require('./handleListen');
 
 const PORT = 3003;
-const app = express();
-app.get('/', hello);
 app.listen(PORT, handleListen(console.log, PORT));
