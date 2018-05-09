1c97f1bd74099f7026606fe852858380ac29039e
furixturi <alabebop@gmail.com>
2018-05-09 21:29:48 +0900

    generate an ObjectID by ourselves

--- 07-node-todo-api/playground/mongodb-connect.js
+++ 07-node-todo-api/playground/mongodb-connect.js
@@ -1,4 +1,7 @@
-const MongoClient = require("mongodb").MongoClient;
+const { MongoClient, ObjectID } = require("mongodb");
+
+const obj = new ObjectID();
+console.log(obj);
 
 MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
   if (err) {
@@ -20,21 +23,21 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
   //   }
   // );
 
-  db.collection("Users").insertOne(
-    {
-      name: "Charlie Brown",
-      age: 12,
-      location: "London"
-    },
-    (err, result) => {
-      if (err) {
-        return console.log("Unable to insert user", err);
-      }
+  // db.collection("Users").insertOne(
+  //   {
+  //     name: "Charlie Brown",
+  //     age: 12,
+  //     location: "London"
+  //   },
+  //   (err, result) => {
+  //     if (err) {
+  //       return console.log("Unable to insert user", err);
+  //     }
 
-      console.log(JSON.stringify(result.ops, undefined, 2));
-      console.log(result.ops[0]._id.getTimestamp());
-    }
-  );
+  //     console.log(JSON.stringify(result.ops, undefined, 2));
+  //     console.log(result.ops[0]._id.getTimestamp());
+  //   }
+  // );
 
   db.close();
 });
